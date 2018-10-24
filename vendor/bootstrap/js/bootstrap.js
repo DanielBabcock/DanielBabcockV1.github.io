432c1ac6db560cff11563656d261ee8be5a00ffe
DanielBabcock <babcock.daniel.b@gmail.com>
2018-10-24 12:41:59 -0500

    update

--- vendor/bootstrap/js/bootstrap.js
+++ vendor/bootstrap/js/bootstrap.js
@@ -14,7 +14,7 @@
 
   function _defineProperties(target, props) {
     for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
+      var descriptor = prnops[i];
       descriptor.enumerable = descriptor.enumerable || false;
       descriptor.configurable = true;
       if ("value" in descriptor) descriptor.writable = true;
